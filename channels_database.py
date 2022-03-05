d44f818150db75da768dec655d423997860933db
Beata <dimon2000.1982@gmail.com>
2022-03-05 17:52:18 +0200

    Add google sheets as second source, add reported file as checkpoint

--- channels_database.py
+++ channels_database.py
@@ -1,6 +1,10 @@
 
 import random
 import pandas as pd
+from googleapiclient.discovery import build
+
+import httplib2 
+from oauth2client.service_account import ServiceAccountCredentials	
 
 
 class ChannelsDatabase:
@@ -8,19 +12,45 @@ class ChannelsDatabase:
         self._db_main = pd.read_csv(path)
         self._db_main.sort_values(by=['priority'])
 
-        self._db_grouped = self._db_main.groupby(['priority'], as_index=False)['channel'].agg(lambda x: list(x))
+        self.CREDENTIALS_FILE = 'cleaner-342521-ec5546fa5567.json'
 
-    def get_priorities(self):
-        return list(self._db_grouped['priority'])
+        self.credentials = ServiceAccountCredentials.from_json_keyfile_name(self.CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
 
-    def channels_iterator(self, priority, shuffle=False):
-        channels = self._db_grouped[self._db_grouped['priority'] == priority]['channel'].tolist()[0]
+        self.httpAuth = self.credentials.authorize(httplib2.Http())
+        self.driveService = build('drive', 'v2', http = self.httpAuth)
+        self.service = build('sheets', 'v4', http = self.httpAuth)
+
+    def channels_iterator(self, shuffle=False):
+        table_channels = self.get_from_table()
+        channels = self._db_main['channel'].tolist()
+        channels = [item.strip() for item in channels]
+        channels +=[self.clean_hash(item) for item in table_channels if self.clean_hash(item) not in channels]
+        print("Кількість каналів: "+ str(len(channels)))
+        reported = self.read_reported()
+        channels = [item for item in channels if item not in reported]
+        print("Кількість каналів, на які ще не відправлена скарга: "+ str(len(channels)))
         if shuffle:
             random.shuffle(channels)
         for telegram_channel in channels:
-            if "https://" in telegram_channel:
-                telegram_channel = telegram_channel.split('/')[-1]
-            elif '@' in telegram_channel:
-                telegram_channel = telegram_channel[1:]
             yield telegram_channel
-
+    def get_from_table(self, link='https://docs.google.com/spreadsheets/d/1UXsdKXaSWkkdCYlJJQ3Elv6NgPEmQ-wbVBTvgLV90Ss/edit#gid=0'):
+        ranges = ["Telegram!A1:A99999"]
+        dest_id = link.split('/')[5]
+        results = self.service.spreadsheets().values().batchGet(spreadsheetId = dest_id, 
+                                                ranges = ranges,
+                                                majorDimension='COLUMNS', 
+                                                valueRenderOption = 'FORMATTED_VALUE',  
+                                                dateTimeRenderOption = 'FORMATTED_STRING').execute()
+        if results['valueRanges'][0].get('values'):
+            values= results['valueRanges'][0]['values'][0]
+        return values
+    def clean_hash(self, link):
+        telegram_channel = link
+        if "https://" in link:
+            telegram_channel = link.split('/')[-1]
+        elif '@' in link:
+            telegram_channel = link[1:]
+        return telegram_channel
+    def read_reported(self):
+        db_main = pd.read_csv('reported.csv')
+        return db_main['reported'].values.tolist()
--- report_channels.py
+++ report_channels.py
@@ -9,40 +9,46 @@ from telethon import functions, types
 from report_message_generator import generate_report_message
 from client_factory import ClientFactory
 from channels_database import ChannelsDatabase
-
+from csv import writer
 
 client = ClientFactory.create_client(session_name="report")
 print('Bot started')
 
 
 async def main():
-    number_of_channels_rep = 150
+    # тут задаєте кількість каналів, після яких програма закінчить надсилати скарги
+    number_of_channels_rep = 1000
     number_completed = 0
     channels_db = ChannelsDatabase()
 
     while number_completed < number_of_channels_rep:
-        for priority in channels_db.get_priorities():
-            for telegram_channel in channels_db.channels_iterator(priority):
-                try:
-                    result = await client(functions.account.ReportPeerRequest(
-                        peer=telegram_channel,
-                        reason=types.InputReportReasonSpam(),
-                        message=generate_report_message())
-                    )
-                    print(telegram_channel.strip(), result)
-                except ValueError:
-                    print("Channel not found")
-                    number_completed -= 1
-                except errors.UsernameInvalidError:
-                    print("Nobody is using this username, or the username is unacceptable")
-                except errors.FloodWaitError as e:
-                    seconds_left = e.seconds
-                    while seconds_left > 0:
-                        print("Flood wait error. Waiting for ", str(datetime.timedelta(seconds=seconds_left)))
-                        seconds_left -= 60
-                        await asyncio.sleep(60)
-                number_completed += 1
-                await asyncio.sleep(20 + 2 * random.random())
+        for telegram_channel in channels_db.channels_iterator():
+            try:
+                result = await client(functions.account.ReportPeerRequest(
+                    peer=telegram_channel,
+                    reason=types.InputReportReasonSpam(),
+                    message=generate_report_message())
+                )
+                print(telegram_channel.strip(), result)
+            except ValueError:
+                print("Channel not found")
+                number_completed -= 1
+            except errors.UsernameInvalidError:
+                print("Nobody is using this username, or the username is unacceptable")
+            except errors.FloodWaitError as e:
+                seconds_left = e.seconds
+                while seconds_left > 0:
+                    print("Flood wait error. Waiting for ", str(datetime.timedelta(seconds=seconds_left)))
+                    seconds_left -= 60
+                    await asyncio.sleep(60)
+            number_completed += 1
+            with open('reported.csv', 'a', newline='\n') as f_object:
+                writer_obj = writer(f_object)
+                writer_obj.writerow([telegram_channel])
+                f_object.close()
+            # замість 10 можна написати меншу або більшу затримку, що пришвидшить надсилання скарг,
+            #  але можливо збільшить шанс бану (можливо бан за таке не дають, але лишній захист не зашкодить)
+            await asyncio.sleep(10 + 2 * random.random())
 
 
 with client:
